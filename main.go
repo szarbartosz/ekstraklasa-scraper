f4464dd68f59190702e6f6a902f2ac5ffaeb531c
Bartosz Szar <szarbartosz@gmail.com>
2024-02-12 20:11:10 +0100

    add mocked table response

--- main.go
+++ main.go
@@ -2,11 +2,28 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type Standings struct {
+	Position string
+	Team     string
+	Points   int
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/table", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"standings": []Standings{
+				{
+					Position: "1",
+					Team:     "Team A",
+					Points:   45,
+				},
+				{
+					Position: "2",
+					Team:     "Team B",
+					Points:   43,
+				},
+			},
 		})
 	})
 	r.Run()
