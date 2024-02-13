2667dfebc24241863f93b665a67d7b33e6036df1
Krzysztof Nalepa <kraleppa@gmail.com>
2024-02-13 12:53:31 +0100

    Added proper scraping

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -10,29 +10,35 @@ import (
 )
 
 func Scrape() {
-	c := colly.NewCollector()
+	collector := colly.NewCollector()
 
-	c.OnHTML("tr", func(e *colly.HTMLElement) {
-		e.ForEach("td", func(i int, el *colly.HTMLElement) {
-			testID := el.Attr("data-testid")
-			fmt.Println(testID)
-			if testID == "atom-table-cell-last-results" {
-				el.ForEach("svg", func(i int, el *colly.HTMLElement) {
-					fmt.Println(el.Attr("class"))
-				})
-			} else if testID == "table-cell-team" {
-				el.ForEach("a", func(i int, el *colly.HTMLElement) {
-					fmt.Println(el.Text)
-				})
-			} else if testID == "table-cell-icon" {
-				fmt.Println(el)
-				el.ForEach("source", func(i int, el *colly.HTMLElement) {
-					fmt.Println(el.Attr("srcset"))
-				})
+	collector.OnHTML("tr", func(tr *colly.HTMLElement) {
+		tr.ForEach("td", func(i int, td *colly.HTMLElement) {
+			if td.Attr("data-testid") == "table-cell-value" && i == 1 {
+				fmt.Println("Team position", td.Text)
+			} else if td.Attr("data-testid") == "table-cell-team" && i == 3 {
+				fmt.Println("Team name", td.ChildText("a.hidden"))
+			} else if td.Attr("data-testid") == "atom-table-cell-last-results" && i == 4 {
+				// TODO Handle these nice svgs
+			} else if td.Attr("data-testid") == "table-cell-value" && i == 5 {
+				fmt.Println("Games played", td.Text)
+			} else if td.Attr("data-testid") == "table-cell-value" && i == 6 {
+				fmt.Println("Wins", td.Text)
+			} else if td.Attr("data-testid") == "table-cell-value" && i == 7 {
+				fmt.Println("Draws", td.Text)
+			} else if td.Attr("data-testid") == "table-cell-value" && i == 8 {
+				fmt.Println("Losses", td.Text)
+			} else if td.Attr("data-testid") == "table-cell-value" && i == 9 {
+				fmt.Println("Goals for", td.Text)
+			} else if td.Attr("data-testid") == "table-cell-value" && i == 10 {
+				fmt.Println("Goals against", td.Text)
+			} else if td.Attr("data-testid") == "table-cell-value" && i == 11 {
+				fmt.Println("Goals difference", td.Text)
+			} else if td.Attr("data-testid") == "table-cell-value" && i == 12 {
+				fmt.Println("Team points", td.Text)
 			}
 		})
-
-		fmt.Println(e.Attr("table-cell-team"))
+		fmt.Println("\n")
 	})
 
 	err := godotenv.Load()
@@ -42,5 +48,5 @@ func Scrape() {
 
 	scrapeUrl := os.Getenv("EKSTRAKLASA_URL")
 
-	c.Visit(scrapeUrl)
+	collector.Visit(scrapeUrl)
 }
