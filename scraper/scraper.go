bc4965feaaf5aa6b411929020930b038591dd053
Krzysztof Nalepa <56135216+kraleppa@users.noreply.github.com>
2024-02-13 17:26:40 +0100

    Return actual results in GET `/table` response (#2)
    * Scraper returning slice of standings
    
    * Passing standings to response
    
    * Added `ParseToInt` function

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,46 +1,56 @@
 package scraper
 
 import (
-	"fmt"
 	"log"
 	"os"
+	"strconv"
+
+	"scraper/ekstraklasa/models"
 
 	"github.com/gocolly/colly"
 	"github.com/joho/godotenv"
 )
 
-func Scrape() {
+func Scrape() []models.Standing {
 	collector := colly.NewCollector()
+	headerSkipped := false
+
+	var standings []models.Standing
 
 	collector.OnHTML("tr", func(tr *colly.HTMLElement) {
-		tr.ForEach("td", func(i int, td *colly.HTMLElement) {
-			switch i {
-			case 1:
-				fmt.Println("Team position:", td.Text)
-			case 3:
-				fmt.Println("Team name:", td.ChildText("a.hidden"))
-			case 4:
-				// TODO Handle these nice svgs
-			case 5:
-				fmt.Println("Games played:", td.Text)
-			case 6:
-				fmt.Println("Wins:", td.Text)
-			case 7:
-				fmt.Println("Draws:", td.Text)
-			case 8:
-				fmt.Println("Losses:", td.Text)
-			case 9:
-				fmt.Println("Goals for:", td.Text)
-			case 10:
-				fmt.Println("Goals against:", td.Text)
-			case 11:
-				fmt.Println("Goals difference:", td.Text)
-			case 12:
-				fmt.Println("Team points:", td.Text)
-			}
+		standing := models.Standing{}
 
-		})
-		fmt.Println("\n")
+		if headerSkipped {
+			tr.ForEach("td", func(i int, td *colly.HTMLElement) {
+				switch i {
+				case 1:
+					standing.Position = ParseToInt(td.Text)
+				case 3:
+					standing.TeamName = td.ChildText("a.hidden")
+				case 4:
+					// TODO Handle these nice svgs
+				case 5:
+					standing.GamesPlayed = ParseToInt(td.Text)
+				case 6:
+					standing.Wins = ParseToInt(td.Text)
+				case 7:
+					standing.Draws = ParseToInt(td.Text)
+				case 8:
+					standing.Losses = ParseToInt(td.Text)
+				case 9:
+					standing.GoalsFor = ParseToInt(td.Text)
+				case 10:
+					standing.GoalsAgainst = ParseToInt(td.Text)
+				case 11:
+					standing.GoalsDifference = ParseToInt(td.Text)
+				case 12:
+					standing.TeamPoints = ParseToInt(td.Text)
+				}
+			})
+			standings = append(standings, standing)
+		} else {
+			headerSkipped = true
+		}
 	})
 
 	err := godotenv.Load()
@@ -51,4 +61,19 @@ func Scrape() {
 	scrapeUrl := os.Getenv("EKSTRAKLASA_URL")
 
 	collector.Visit(scrapeUrl)
+
+	return standings
+}
+
+func ParseToInt(toBeParsed string) int {
+	var parsed int
+	var err error
+
+	parsed, err = strconv.Atoi(toBeParsed)
+
+	if err != nil {
+		log.Fatal("Error while parsing to int: ", err)
+	}
+
+	return parsed
 }
