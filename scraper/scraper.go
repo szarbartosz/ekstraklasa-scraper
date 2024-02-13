4b79643676f18cbf9c803c26d797bb8f71c95ed3
Krzysztof Nalepa <kraleppa@gmail.com>
2024-02-13 17:23:39 +0100

    Added `ParseToInt` function

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -13,9 +13,9 @@ import (
 
 func Scrape() []models.Standing {
 	collector := colly.NewCollector()
-	var err error
-	var standings []models.Standing
 	headerSkipped := false
+
+	var standings []models.Standing
 
 	collector.OnHTML("tr", func(tr *colly.HTMLElement) {
 		standing := models.Standing{}
@@ -24,40 +24,36 @@ func Scrape() []models.Standing {
 			tr.ForEach("td", func(i int, td *colly.HTMLElement) {
 				switch i {
 				case 1:
-					standing.Position, err = strconv.Atoi(td.Text)
+					standing.Position = ParseToInt(td.Text)
 				case 3:
 					standing.TeamName = td.ChildText("a.hidden")
 				case 4:
 					// TODO Handle these nice svgs
 				case 5:
-					standing.GamesPlayed, err = strconv.Atoi(td.Text)
+					standing.GamesPlayed = ParseToInt(td.Text)
 				case 6:
-					standing.Wins, err = strconv.Atoi(td.Text)
+					standing.Wins = ParseToInt(td.Text)
 				case 7:
-					standing.Draws, err = strconv.Atoi(td.Text)
+					standing.Draws = ParseToInt(td.Text)
 				case 8:
-					standing.Losses, err = strconv.Atoi(td.Text)
+					standing.Losses = ParseToInt(td.Text)
 				case 9:
-					standing.GoalsFor, err = strconv.Atoi(td.Text)
+					standing.GoalsFor = ParseToInt(td.Text)
 				case 10:
-					standing.GoalsAgainst, err = strconv.Atoi(td.Text)
+					standing.GoalsAgainst = ParseToInt(td.Text)
 				case 11:
-					standing.GoalsDifference, err = strconv.Atoi(td.Text)
+					standing.GoalsDifference = ParseToInt(td.Text)
 				case 12:
-					standing.TeamPoints, err = strconv.Atoi(td.Text)
+					standing.TeamPoints = ParseToInt(td.Text)
 				}
 			})
-			if err != nil {
-				log.Fatal("Error while parsing standing: ", err)
-			} else {
-				standings = append(standings, standing)
-			}
+			standings = append(standings, standing)
 		} else {
 			headerSkipped = true
 		}
 	})
 
-	err = godotenv.Load()
+	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
@@ -67,4 +63,17 @@ func Scrape() []models.Standing {
 	collector.Visit(scrapeUrl)
 
 	return standings
+}
+
+func ParseToInt(toBeParsed string) int {
+	var parsed int
+	var err error
+
+	parsed, err = strconv.Atoi(toBeParsed)
+
+	if err != nil {
+		log.Fatal("Error while parsing to int: ", err)
+	}
+
+	return parsed
 }
