d19df094ec9113f5e97c084b5587cad1242b6a65
Krzysztof Nalepa <kraleppa@gmail.com>
2024-02-13 13:01:32 +0100

    Changed `ifs` to `switch`

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -14,29 +14,31 @@ func Scrape() {
 
 	collector.OnHTML("tr", func(tr *colly.HTMLElement) {
 		tr.ForEach("td", func(i int, td *colly.HTMLElement) {
-			if td.Attr("data-testid") == "table-cell-value" && i == 1 {
-				fmt.Println("Team position", td.Text)
-			} else if td.Attr("data-testid") == "table-cell-team" && i == 3 {
-				fmt.Println("Team name", td.ChildText("a.hidden"))
-			} else if td.Attr("data-testid") == "atom-table-cell-last-results" && i == 4 {
+			switch i {
+			case 1:
+				fmt.Println("Team position:", td.Text)
+			case 3:
+				fmt.Println("Team name:", td.ChildText("a.hidden"))
+			case 4:
 				// TODO Handle these nice svgs
-			} else if td.Attr("data-testid") == "table-cell-value" && i == 5 {
-				fmt.Println("Games played", td.Text)
-			} else if td.Attr("data-testid") == "table-cell-value" && i == 6 {
-				fmt.Println("Wins", td.Text)
-			} else if td.Attr("data-testid") == "table-cell-value" && i == 7 {
-				fmt.Println("Draws", td.Text)
-			} else if td.Attr("data-testid") == "table-cell-value" && i == 8 {
-				fmt.Println("Losses", td.Text)
-			} else if td.Attr("data-testid") == "table-cell-value" && i == 9 {
-				fmt.Println("Goals for", td.Text)
-			} else if td.Attr("data-testid") == "table-cell-value" && i == 10 {
-				fmt.Println("Goals against", td.Text)
-			} else if td.Attr("data-testid") == "table-cell-value" && i == 11 {
-				fmt.Println("Goals difference", td.Text)
-			} else if td.Attr("data-testid") == "table-cell-value" && i == 12 {
-				fmt.Println("Team points", td.Text)
+			case 5:
+				fmt.Println("Games played:", td.Text)
+			case 6:
+				fmt.Println("Wins:", td.Text)
+			case 7:
+				fmt.Println("Draws:", td.Text)
+			case 8:
+				fmt.Println("Losses:", td.Text)
+			case 9:
+				fmt.Println("Goals for:", td.Text)
+			case 10:
+				fmt.Println("Goals against:", td.Text)
+			case 11:
+				fmt.Println("Goals difference:", td.Text)
+			case 12:
+				fmt.Println("Team points:", td.Text)
 			}
+
 		})
 		fmt.Println("\n")
 	})
