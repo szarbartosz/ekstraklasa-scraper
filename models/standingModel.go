0c58bf996d1fe0c315ebda08fbc7a86677ce8494
Bartosz Szar <szarbartosz@gmail.com>
2024-02-15 16:27:30 +0100

    update standing model properties name

--- models/standingModel.go
+++ models/standingModel.go
@@ -1,16 +1,16 @@
 package models
 
 type Standing struct {
-	Position        int
-	TeamName        string
-	LogoUrl         string
-	LastResults     []MatchResult
-	GamesPlayed     int
-	Wins            int
-	Draws           int
-	Losses          int
-	GoalsFor        int
-	GoalsAgainst    int
-	GoalsDifference int
-	TeamPoints      int
+	Position        int           `json:"position"`
+	TeamName        string        `json:"teamName"`
+	LogoUrl         string        `json:"logoUrl"`
+	LastResults     []MatchResult `json:"lastResults"`
+	GamesPlayed     int           `json:"gamesPlayed"`
+	Wins            int           `json:"wins"`
+	Draws           int           `json:"draws"`
+	Losses          int           `json:"losses"`
+	GoalsFor        int           `json:"goalsFor"`
+	GoalsAgainst    int           `json:"goalsAgainst"`
+	GoalsDifference int           `json:"goalsDifference"`
+	TeamPoints      int           `json:"teamPoints"`
 }
