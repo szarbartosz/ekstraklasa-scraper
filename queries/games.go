1c2551674535a3dbc5f811e79e161e78899ef403
Krzysztof Nalepa <kraleppa@gmail.com>
2024-03-07 21:02:20 +0100

    Added filter for host

--- queries/games.go
+++ queries/games.go
@@ -5,14 +5,39 @@ import (
 	"os"
 	"scraper/ekstraklasa/models"
 	"scraper/ekstraklasa/scrapers/gol"
+	"scraper/ekstraklasa/utils"
+	"strings"
 )
 
 func QueryGames(queryParams map[string][]string) []models.Game {
-	scrapeUrl := os.Getenv("GOL_GAMES_URL")
+	scrapeUrl := os.Getenv("GOL_URL")
 
 	if scrapeUrl == "" {
-		log.Panic("No GOL_GAMES_URL env variable found!")
+		log.Panic("No GOL_URL env variable found!")
 	}
 
-	return gol.ScrapeGames(scrapeUrl + "/terminarz")
+	games := gol.ScrapeGames(scrapeUrl + "/terminarz")
+	games = FilterByHost(games, queryParams)
+
+	return games
+}
+
+func FilterByHost(games []models.Game, queryParams map[string][]string) []models.Game {
+	hostNameParam := queryParams["host"]
+
+	if len(hostNameParam) == 0 {
+		return games
+	}
+
+	hostName := hostNameParam[0]
+
+	var filteredGames []models.Game
+	for _, game := range games {
+
+		if strings.Contains(utils.SanitizeString(game.Host), utils.SanitizeString(hostName)) {
+			filteredGames = append(filteredGames, game)
+		}
+	}
+
+	return filteredGames
 }
